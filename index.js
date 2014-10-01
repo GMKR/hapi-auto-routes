c3abfd207216c9b02332b1e02335579c594097a1
Mohan.G <mohan@qshark.com>
2014-10-01 09:47:49 +0530

    added code.
    Signed-off-by: Mohan.G <mohan@qshark.com>

---
+++ index.js
@@ -0,0 +1,21 @@
+var glob = require('glob');
+
+exports.bind = function (server) {
+  this.server = server;
+  return this;
+};
+
+exports.register = function (options) {
+  var self = this;
+  options = options || {};
+  if (!options.pattern) {
+    throw new Error('Specify a matching pattern');
+  }
+
+  glob.sync(options.pattern).forEach(function (file) {
+    var route = require(route);
+    self.server.route(route);
+  });
+
+  return this;
+}
